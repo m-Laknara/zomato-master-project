ff066a923e6482bb5d886d27d411d01db4658029
m-Laknara <minsadilaknara@gmail.com>
2021-10-10 19:40:11 +0530

    google authentication done

--- server/index.js
+++ server/index.js
@@ -4,9 +4,10 @@ require("dotenv").config();
 import  express  from "express";
 import cors from "cors";
 import helmet from "helmet";
+import passport from "passport";
 
 import Auth from "./API/Auth";
-
+import googleAuthConfig from "./config/google.config";
 //database connection
 import ConnectDB from "./database/connection";
 
@@ -16,7 +17,12 @@ zomato.use(express.json());
 zomato.use(express.urlencoded({ extended: false}));
 zomato.use(helmet());
 zomato.use(cors());
+//zomato.use(passport.initialize());
+//zomato.use(passport.session());
 
+
+//passport configuration
+googleAuthConfig(passport);
 //application routes
 zomato.use("/auth",Auth);
 
