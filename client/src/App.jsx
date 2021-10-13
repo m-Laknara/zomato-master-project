e2da8a602e4e24e16cc67ca44f93b2ed957e0397
m-Laknara <minsadilaknara@gmail.com>
2021-10-14 02:18:45 +0530

    Nav Bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
+//HOC
+import HomeLayoutHOC from "./HOC/Home.HOC";
 
+//component
+import Temp from "./Components/temp";
 
 function App() {
-  return <div></div>
+  return(
+    <>
+      <HomeLayoutHOC path="/" exact component={Temp}/>
+    </>
+  ); 
 }
 
 export default App;
---
+++ client/src/Components/temp.jsx
@@ -0,0 +1,8 @@
+//HOC
+import React from "react";
+
+const Temp = () => {
+  return <div>hello this is a temp</div>;
+};
+
+export default Temp;
