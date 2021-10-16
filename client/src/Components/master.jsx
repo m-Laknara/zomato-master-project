ec928bf61bd98b5bf3f32f5eb8e3728dcaa31a3c
m-Laknara <minsadilaknara@gmail.com>
2021-10-16 20:54:13 +0530

    zomato master project

--- client/src/Components/master.jsx
+++ client/src/Components/master.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+
+// components
 import Delivery from "./Delivery";
+import Dining from "./Dining";
+import NightLife from "./NightLife";
+import Nutrition from "./Nutition";
 
 const Master = () => {
-    const { type } = useParams();
-
-    return <div className="my-5">{type === "Delivery" && <Delivery />}</div>;
+  const { type } = useParams();
+  return (
+    <div className="my-5">
+      {type === "Delivery" && <Delivery />}
+      {type === "Dining%20Out" && <Dining />}
+      {type === "Nightlife" && <NightLife />}
+      {type === "Nutrition" && <Nutrition />}
+    </div>
+  );
 };
 
 export default Master;
